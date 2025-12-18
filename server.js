e914f5a50f669745b8c71f62c633e22a940e1adb
Aditya Bhargav <bhargavaditya524@gmail.com>
2025-12-18 17:51:56 +0530

    Update CORS origin for Netlify frontend

--- server.js
+++ server.js
@@ -1,48 +1,3 @@
-// import dotenv from "dotenv";
-// import express from "express";
-// import Razorpay from "razorpay";
-
-// /* Load env variables */
-// dotenv.config({ path: "./config/config.env" });
-
-// const app = express();
-
-
-// /* Razorpay instance */
-// const razorpay = new Razorpay({
-//   key_id: process.env.RAZORPAY_KEY_ID,
-//   key_secret: process.env.RAZORPAY_KEY_SECRET,
-// });
-
-// /* Server */
-// app.listen(process.env.PORT || 4000, () => {
-//   console.log(`Server running on port ${process.env.PORT || 4000}`);
-// });
-
-// export { razorpay as instance };
-
-
-
-
-
-// import express from "express";
-// import paymentRoutes from "./routes/paymentRoutes.js";
-// import cors from "cors";
-// const app = express();
-
-// app.use(express.json());
-
-// /* Add this line */
-// app.use("/api", paymentRoutes);
-
-// app.listen(process.env.PORT || 4000, () => {
-//   console.log(`Server running on port ${process.env.PORT || 4000}`);
-// });
-
-
-
-
-
 
 
 
@@ -55,7 +10,7 @@ const app = express();
 
 // 🔹 Enable CORS globally, must be BEFORE routes
 app.use(cors({
-  origin: "http://localhost:5173", // your React frontend
+  origin: "https://bharadwajayurveda.netlify.app",// your React frontend
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
